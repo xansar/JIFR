663b281b377388603337b4cbfa0ac5be68496348
xansar <xxzhang16@fudan.edu.cn>
2022-12-09 14:57:49 +0800

    超参调整，增加njob参数，可以支持多线程，但是此时需要关闭log打印，否则有bug

--- hyper_param_tune.py
+++ hyper_param_tune.py
@@ -21,35 +21,40 @@ from optuna.visualization import plot_contour    # 等高线
 import os
 from run import run, get_config, parse_args, HiddenPrints
 import random
+import numpy as np
+import time
+import threading
 
 # 设置全局变量
 SEED = 2022  # 占位，会重新抽
 ARGS = None
+mu = threading.Lock()
 
 def objective(trial):
     global ARGS, EPOCH, SEED
     config = get_config(ARGS)
-    # 重新抽取randomseed
-    SEED = random.randint(0, 999999)
-    config['TRAIN']['random_seed'] = str(SEED)
-    random.seed(SEED)
+
     # 设置需要搜索的参数及搜索空间
-    config['OPTIM']['embedding_learning_rate'] = str(trial.suggest_float('embedding_learning_rate', 1e-3, 1e0, log=True))
-    config['OPTIM']['embedding_weight_decay'] = str(trial.suggest_float('embedding_weight_decay', 1e-3, 1e1, log=True))
+    config['OPTIM']['embedding_learning_rate'] = str(trial.suggest_float('embed_lr', 1e-3, 1e0, log=True))
+    config['OPTIM']['embedding_weight_decay'] = str(trial.suggest_float('embed_w', 1e-3, 1e1, log=True))
     if 'mlp_weight_decay' in config['OPTIM'].keys():    # 不对mlp embedding做正则化
-        config['OPTIM']['mlp_learning_rate'] = str(trial.suggest_float('mlp_learning_rate', 1e-3, 1e0, log=True))
+        config['OPTIM']['mlp_learning_rate'] = str(trial.suggest_float('mlp_lr', 1e-3, 1e0, log=True))
     if 'lamda_t' in config['OPTIM'].keys():    # trustsvd link loss 系数
         config['OPTIM']['lamda_t'] = str(trial.suggest_float('lamda_t', 1e-2, 1e1, log=True))
 
+    # 重新抽取randomseed，根据这里的采样抽，防止重复
+    config['TRAIN']['random_seed'] = str(int(eval(config['OPTIM']['embedding_weight_decay']) * 1561365))
+    # random.seed(SEED)
+
     # 学习率只衰减不重启
-    config['OPTIM']['T_0'] = str(config['TRAIN']['epoch'])
+    config['OPTIM']['T_0'] = str(15)
     # 调参时不打印训练信息
     config['LOG'] = False
     with HiddenPrints():
         metric_value = run(config, trial=trial)
     return metric_value
 
-def single_search(model_name, n_trials):
+def single_search(model_name, n_trials, n_jobs):
     global SEED
     study = optuna.create_study(
         sampler=optuna.samplers.TPESampler(
@@ -64,7 +69,7 @@ def single_search(model_name, n_trials):
         direction='maximize',
         study_name=model_name,
     )
-    study.optimize(objective, n_trials=n_trials)
+    study.optimize(objective, n_trials=n_trials, n_jobs=n_jobs)
     return study
 
 def visulization(study, model_name, data_name, save_dir=None):
@@ -76,42 +81,62 @@ def visulization(study, model_name, data_name, save_dir=None):
         'contour',
     ]
     save_pth = os.path.join(save_dir, model_name, data_name)
-    for func in funcs:
-        fig = eval('plot_' + func)(study)
-        if not os.path.isdir(save_pth):
-            os.makedirs(save_pth)
-        # 保存图片
-        fig.write_image(os.path.join(save_pth, f'{func}.jpg'), format='jpg')
+    # for func in funcs:
+    #     fig = eval('plot_' + func)(study)
+    #     if not os.path.isdir(save_pth):
+    #         os.makedirs(save_pth)
+    #     # 保存图片
+    #     fig.write_image(os.path.join(save_pth, f'{func}.jpg'), format='jpg')
     # 写入最优参数
-    with open(os.path.join(save_pth, 'best_params.txt'), 'w') as f:
+    with open(os.path.join(save_pth, f'best_params.txt'), 'w') as f:
         for k, v in study.best_params.items():
             f.write(f'{k}: {v}\n')
-            print(f'{k}: {v}')
+            # print(f'{k}: {v}')
         f.write(f'best nDCG: {study.best_value}\n')
-        print(f'best nDCG: {study.best_value}')
+        # print(f'best nDCG: {study.best_value}')
+        f.close()
 
-def search(model_name, data_name, epoch, batch_size, n_trials):
+
+def search(model_name, data_name, epoch, train_batch_size, eval_batch_size, n_trials, n_jobs):
     global ARGS
     ARGS = parse_args()
     ARGS.model_name = model_name
     ARGS.data_name = data_name
     ARGS.epoch = epoch
-    ARGS.train_batch_size = batch_size
-    ARGS.eval_batch_size = batch_size
+    ARGS.train_batch_size = train_batch_size
+    ARGS.eval_batch_size = eval_batch_size
 
-    study = single_search(model_name, n_trials)
-    visulization(study, model_name=model_name, data_name=data_name, save_dir='./params_search')
-    print(
-        "Remember to clean the save dir after params searching!!!"
-    )
+    study = single_search(model_name, n_trials, n_jobs)
+
+    if mu.acquire(True):
+        visulization(study, model_name=model_name, data_name=data_name, save_dir='./params_search')
+        mu.release()
+    # print(
+    #     "Remember to clean the save dir after params searching!!!"
+    # )
 
 if __name__ == '__main__':
-    search(
-        model_name='TrustSVD',
-        data_name='Epinions',
-        batch_size=10240,
-        epoch=1,   # 每次实验运行的epoch数
-        n_trials=2
-    )
+    models = ['DiffnetPP']
+    datasets = ['Flickr']
+    for model in models:
+        for dataset in datasets:
+            if not os.path.exists(os.path.join('./log', model, dataset)):
+                os.makedirs(os.path.join('./log', model, dataset))
+            if not os.path.exists(os.path.join('./save', model, dataset)):
+                os.makedirs(os.path.join('./save', model, dataset))
+            if not os.path.exists(os.path.join('./params_search', model, dataset)):
+                os.makedirs(os.path.join('./params_search', model, dataset))
+        search(
+            model_name='DiffnetPP',
+            data_name='Flickr',
+            train_batch_size=10240,
+            eval_batch_size=10240,
+            epoch=16,   # 每次实验运行的epoch数
+            n_trials=3,
+            n_jobs=3
+        )
+    #
+    # import os
+    # os.system('/root/upload.sh')
 
 
